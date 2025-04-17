dc1c185c8a21556ae52d2da7b758eca3bc80135c
Sashreek Shankar <sashreek123@hotmail.com>
2025-04-17 14:04:12 -0400

    X and Y dimmensions work?

--- crazyflie/scripts/iekf.py
+++ crazyflie/scripts/iekf.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-import pathlib
-
 import rclpy
-from rclpy import Parameter
-import numpy as np
-from scipy.linalg import expm,block_diag #block_diag not accessed
-from scipy.spatial.transform import Rotation as R
-from numpy.linalg import norm
 from rclpy.node import Node
+import numpy as np
+from scipy.linalg import expm
+from numpy.linalg import inv
+from scipy.spatial.transform import Rotation as R
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import PoseStamped,Point,Quaternion,Twist,Pose,PoseWithCovariance,TwistWithCovariance
+from geometry_msgs.msg import PoseStamped, Point, Quaternion, Twist, Pose, PoseWithCovariance, TwistWithCovariance
 from nav_msgs.msg import Odometry
-from rclpy.parameter import Parameter
 
 class IEKF(Node):
     def __init__(self):
@@ -20,7 +16,6 @@ class IEKF(Node):
         self.declare_parameters(
             "",
             [
-                # ("imu_topic", "/fake_imu"),
                 ("imu_topic", "/cf_1/imu"),
                 ("pose_topic", "/cf_1/pose"),
                 ("odom_topic", "/cf_1/odom"),
@@ -41,206 +36,230 @@ class IEKF(Node):
         # publishers
         self.pose_pub = self.create_publisher(Odometry, output_topic, 1)
 
-        #timers
+        # timers
         self.ekf_timer = self.create_timer(0.2, self.timer_callback)
 
-        # parameters and system
-        #TODO: initial state needs to be set correctly
-        self.Pos = np.zeros((3,1),dtype=np.float64)                                               # state position
-        self.R = np.eye(3,dtype=np.float64)                                                       # state orientation
-        self.vel = np.zeros((3,1),dtype=np.float64)                                               # velocity
-        self.X = np.eye(5,dtype=np.float64)                                                   # combined state vector
+        self.R = np.eye(3)                                # Rotation matrix (orientation)
+        self.v = np.zeros((3, 1))                         # Velocity
+        self.p = np.zeros((3, 1))                         # Position
+        self.X = self.compose_state_matrix(self.R, self.v, self.p)  # Combined state matrix
 
-        #TODO:block diag of cov_omega,cov_accel,cov_position and these should be fixed values
-        self.cov_Pos = .1 * np.eye(3,dtype=np.float64)                                            # covariance for position
-        self.cov_accel = .1 * np.eye(3,dtype=np.float64)                                         # covariance for accel
-        self.cov_omega = .1 * np.eye(3,dtype=np.float64)                                         # covariance for ang vel
+        self.P = np.eye(9) * 0.1                          # Error state covariance (9x9)
+        self.Q = np.eye(9) * 0.01                         # Process noise covariance (9x9)
+        self.N = np.eye(3) * 0.1                          # Measurement noise covariance (3x3)
 
-        self.cov_omega_noise= .1*np.random.normal(loc=0,scale=1,size=(3,3))
-        self.cov_accel_noise = .1*np.random.normal(loc=0,scale=1,size=(3,3))
-        self.cov_Pos_noise = .1*np.random.normal(loc=0,scale=1,size=(3,3))
-        self.P = block_diag(self.cov_omega, self.cov_accel,self.cov_Pos)         #combined cov
-        self.Q = block_diag(self.cov_omega_noise, self.cov_accel_noise,self.cov_Pos_noise)                      #process noise covariance
-        self.N = np.eye(3,dtype=np.float64)                                                       #GPS noise covariance
-
-        self.prev_t = 0.0
-        self.g = np.zeros((3,1),dtype=np.float64)           #gravity vector (should be in body frame)
-        self.g[2] = -9.81       
-        self.init_time_bool = False
-
-    def imu_callback(self, imu_msg: Imu): #prediction
-        # You can see the values if you run the sim and "gz topic --echo --topic /cf_0/imu"
-        omega = np.array([imu_msg.angular_velocity.x,
-                      imu_msg.angular_velocity.y,
-                      imu_msg.angular_velocity.z]).reshape((3, 1))
-        accel = np.array([imu_msg.linear_acceleration.x,
-                      imu_msg.linear_acceleration.y,
-                      imu_msg.linear_acceleration.z]).reshape((3, 1))
+        self.g = np.array([[0], [0], [-9.81]])
         
-        #skip first iteration to get prev_time
+
+        self.prev_t = None
+        self.get_logger().info("IEKF initialized")
+
+    def imu_callback(self, imu_msg: Imu):
+        """
+        Propagation step using IMU measurements (angular velocity and acceleration)
+        """
+        omega = np.array([[imu_msg.angular_velocity.x], 
+                          [imu_msg.angular_velocity.y], 
+                          [imu_msg.angular_velocity.z]])
+        
+        a = np.array([[imu_msg.linear_acceleration.x], 
+                      [imu_msg.linear_acceleration.y], 
+                      [imu_msg.linear_acceleration.z]])
+        
+        # Get current time
         curr_t = imu_msg.header.stamp.sec + (imu_msg.header.stamp.nanosec * 1e-9)
-        if self.init_time_bool == False:
+        
+        # Skip first iteration to establish time reference
+        if self.prev_t is None:
             self.prev_t = curr_t
-            self.init_time_bool = True
             return
+            
         dt = curr_t - self.prev_t
         self.prev_t = curr_t
+        
+        if dt <= 0:
+            return
+        
+        # PROPAGATION STEP (from the formulation in section 7.2.1)
+        omega_dt = omega * dt
+        R_delta = self.exp_so3(omega_dt)
+        R_new = self.R @ R_delta
+        
+        v_new = self.v + (self.R @ a + self.g) * dt
+        
+        p_new = self.p + self.v * dt + 0.5 * (self.R @ a + self.g) * dt**2
+        
+        # Update state
+        self.R = R_new
+        self.v = v_new
+        self.p = p_new
+        
+        self.X = self.compose_state_matrix(self.R, self.v, self.p)
+        
+        Adg = self.adjoint_SE3(R_delta)
+        
+        self.P = Adg @ self.P @ Adg.T + self.Q
 
-        phi = omega * dt
-        norm_phi = norm(phi)
-        phi_wedge = wedge(phi)
+    def pose_callback(self, pose_msg: PoseStamped):
+        """
+        Update step using position measurements
+        """
+        # Extract position measurement
+        y = np.array([[pose_msg.pose.position.x], 
+                      [pose_msg.pose.position.y], 
+                      [pose_msg.pose.position.z]])
+        
+        b_k = self.R.T @ (y - self.p)
+        
+        H = np.zeros((3, 9))
+        H[:, 6:9] = np.eye(3)
+        S = H @ self.P @ H.T + self.N
+        K = self.P @ H.T @ inv(S)
+        xi = K @ b_k
+        
+        xi_theta = xi[0:3].reshape(3, 1)  # Rotation error
+        xi_v = xi[3:6].reshape(3, 1)      # Velocity error
+        xi_p = xi[6:9].reshape(3, 1)      # Position error
+        
 
-        if norm_phi < 1e-5:
-            gamma0 = np.eye(3) + phi_wedge
-            gamma1 = np.eye(3) + 0.5 * phi_wedge
-            gamma2 = (1/6) * np.eye(3) + (1/24) * phi_wedge
-        else:
-            gamma0 = np.eye(3) + (np.sin(norm_phi) / norm_phi) * phi_wedge + \
-                    ((1 - np.cos(norm_phi)) / norm_phi**2) * (phi_wedge @ phi_wedge)
+        self.R = self.R @ self.exp_so3(xi_theta)
+        
+        self.v = self.v + xi_v
+        
+        self.p = self.p + xi_p
+        
+        self.X = self.compose_state_matrix(self.R, self.v, self.p)
+        
+        I_KH = np.eye(9) - K @ H
+        self.P = I_KH @ self.P @ I_KH.T + K @ self.N @ K.T
 
-            gamma1 = np.eye(3) + ((1 - np.cos(norm_phi)) / norm_phi**2) * phi_wedge + \
-                    ((norm_phi - np.sin(norm_phi)) / norm_phi**3) * (phi_wedge @ phi_wedge)
-
-            gamma2 = 0.5 * np.eye(3) + ((norm_phi - np.sin(norm_phi)) / norm_phi**3) * phi_wedge + \
-                    ((norm_phi**2 + 2 * np.cos(norm_phi) - 2) / (2 * norm_phi**4)) * (phi_wedge @ phi_wedge)
-
-        #predict position
-        self.Pos = self.Pos + self.vel*dt + self.R@gamma2@accel*dt**2 + .5*self.g*dt**2
-        self.vel = self.vel + self.R@ gamma1 @ accel *dt +  self.g*dt
-
-        #predict orientation state
-        self.R = self.R @ expm(wedge(phi))
-
-        #predict cov
-        stm11 = gamma0.T
-        stm21 = -stm11@wedge(gamma1@accel)*dt
-        stm31 = -stm11@wedge(gamma2@accel)*dt**2
-        # stm22 = stm11
-        stm32 = stm11*dt
-        # stm33 = stm11
-        stm = np.block([[stm11,np.zeros((3,3)),np.zeros((3,3))],[stm21,stm11,np.zeros((3,3))],[stm31,stm32,stm11]])
-        self.P = stm@self.P@stm.T + self.Q #adj
-
-        #
-        self.X = wedge_se2_3(self.R,self.vel,self.Pos)
-        return 
-
-    def pose_callback(self, pose_msg: PoseStamped): #correction
-        # TODO: this should eventually be slowed down to publish more infrequently
-
-        #position measurement from GPS
-        yk = np.array([[pose_msg.pose.position.x], [pose_msg.pose.position.y], [pose_msg.pose.position.z],[0.0],[1.0]]) #(5,1)
-        H = np.block([np.zeros((3, 3)), np.zeros((3, 3)), np.eye(3)]) #(3,9) Measurement Jacobian
-        S = H @ self.P @ H.T + self.N #(3,3)
-        L = self.P @ H.T @ np.linalg.inv(S) #(9,3)
-        b = np.zeros((5,1))
-        b[-1] = 1
-        vk = np.linalg.inv(self.X)@yk - b
-        self.X = self.X @ expm(wedge_error(L@vk[:3]))
-
-        #correct individual vars
-        self.R = self.X[0:3, 0:3]
-        self.vel = self.X[0:3, 3].reshape((3,1))
-        self.Pos = self.X[0:3, 4].reshape((3,1))
-        return
-    
     def timer_callback(self):
-
-        self.get_logger().info("me try publish")
-
+        """
+        Publish the current state estimate
+        """
+        # Convert rotation matrix to quaternion
         r = R.from_matrix(self.R)
-
-
-
-        quat = r.as_quat() #should be scalar last (x,y,z,w)
+        quat = r.as_quat()  # (x,y,z,w)
+        
         p = Point()
-        p.x = float(self.Pos[0])
-        p.y = float(self.Pos[1])
-        p.z = float(self.Pos[2])
+        p.x = float(self.p[0])
+        p.y = float(self.p[1])
+        p.z = float(self.p[2])
+        
         q = Quaternion()
         q.x = float(quat[0])
         q.y = float(quat[1])
         q.z = float(quat[2])
         q.w = float(quat[3])
-        cov = np.zeros((6,6)).flatten()
-
+        
         twist = Twist()
         twist.angular.x = 0.0
         twist.angular.y = 0.0
         twist.angular.z = 0.0
-        twist.linear.x = float(self.vel[0])
-        twist.linear.y = float(self.vel[1])
-        twist.linear.z = float(self.vel[2])
-
+        twist.linear.x = float(self.v[0])
+        twist.linear.y = float(self.v[1])
+        twist.linear.z = float(self.v[2])
+        
         pose = Pose()
         pose.orientation = q
         pose.position = p
-
+        
+        pos_cov = self.P[6:9, 6:9]
+        rot_cov = self.P[0:3, 0:3]
+        
+        pose_cov = np.zeros((6, 6))
+        pose_cov[0:3, 0:3] = pos_cov
+        pose_cov[3:6, 3:6] = rot_cov
+        
+        twist_cov = np.zeros((6, 6))
+        twist_cov[0:3, 0:3] = self.P[3:6, 3:6]  # Velocity covariance
+        
         pose_with_cov = PoseWithCovariance()
         pose_with_cov.pose = pose
-        pose_with_cov.covariance = cov #set to zero for now
-
+        pose_with_cov.covariance = pose_cov.flatten().tolist()
+        
         twist_with_cov = TwistWithCovariance()
         twist_with_cov.twist = twist
-        twist_with_cov.covariance = cov #set to zero for now
-
+        twist_with_cov.covariance = twist_cov.flatten().tolist()
+        
         odom_output = Odometry()
         odom_output.pose = pose_with_cov
         odom_output.twist = twist_with_cov
-
-        #TODO: fill header
+        
         odom_output.header.stamp = self.get_clock().now().to_msg()
         odom_output.header.frame_id = "odom"
-
+        odom_output.child_frame_id = "base_link"
+        
         self.pose_pub.publish(odom_output)
-        self.get_logger().info("me publish")
-
 
     def odom_callback(self, odom_msg: Odometry):
-        # This is our "ground truth odometry"
-        # Use it to compare against our IEKF output
-        # TODO: nothing for now
-
-        return
-
-def wedge(phi):
-    """
-    R^3 vector to so(3) matrix
-    @param  phi: R^3
-    @return Phi: so(3) matrix
-    """
-    phi = phi.squeeze()
-    Phi = np.array([[0, -phi[2], phi[1]],
-                    [phi[2], 0, -phi[0]],
-                    [-phi[1], phi[0], 0]])
-    return Phi
-
-def wedge_se2_3(R, v, p):
-    """
-    R matrix, v, p to se2(3) matrix
-    @param  R: Rotation matrix
-    @param  v: velocity vector(3,1)
-    @param  p: position vector(3,1)
-    @return X: se2(3) matrix
-    """
-    X = block_diag(R,np.ones(1),np.ones(1)) #(5,5)
-    X[0:3,3] = v.squeeze()
-    X[0:3,4] = p.squeeze()
-    return X
-def wedge_error(v):
-    rot_err = v[0:3].reshape((3,1))       # rotation error
-    vel_err = v[3:6].reshape((3,1))       # velocity error
-    pos_err = v[6:9].reshape((3,1))       # position error
-
-    rot_err = wedge(rot_err)
-
-    return wedge_se2_3(rot_err,vel_err,pos_err)
-
-def adjoint(R):
-    """
-    Adjoint of SO3 Adjoint (R) = R
-    """
-    return R
+        """
+        Callback for ground truth odometry (for evaluation)
+        """
+        # Not used in this implementation
+        pass
+    
+    def compose_state_matrix(self, R, v, p):
+        """
+        Compose the state matrix X in SE_2(3) from rotation, velocity and position
+        X = [R v p; 0 1 0; 0 0 1]
+        """
+        X = np.eye(5)
+        X[0:3, 0:3] = R
+        X[0:3, 3:4] = v
+        X[0:3, 4:5] = p
+        return X
+    
+    def exp_so3(self, phi):
+        """
+        Exponential map from so(3) to SO(3)
+        Input: 3x1 vector phi (rotation vector)
+        Output: 3x3 rotation matrix
+        """
+        norm_phi = np.linalg.norm(phi)
+        
+        if norm_phi < 1e-6:
+            # Small angle approximation
+            return np.eye(3) + self.skew(phi)
+        
+        axis = phi / norm_phi
+        angle = norm_phi
+        
+        K = self.skew(axis)
+        R = np.eye(3) + np.sin(angle) * K + (1 - np.cos(angle)) * (K @ K)
+        
+        return R
+    
+    def skew(self, v):
+        """
+        Skew-symmetric matrix from 3D vector
+        Input: 3x1 vector v
+        Output: 3x3 skew-symmetric matrix
+        """
+        v = v.flatten()
+        return np.array([
+            [0, -v[2], v[1]],
+            [v[2], 0, -v[0]],
+            [-v[1], v[0], 0]
+        ])
+    
+    def adjoint_SE3(self, R):
+        """
+        Adjoint map for SE(3)
+        Input: 3x3 rotation matrix R
+        Output: 9x9 adjoint matrix (for the error-state)
+        """
+        # For the given IEKF formulation with [R, v, p] state:
+        # The adjoint maps the error state ξ = [ξ_θ, ξ_v, ξ_p]
+        adj = np.zeros((9, 9))
+        
+        adj[0:3, 0:3] = R
+        
+        adj[3:6, 3:6] = R
+        
+        adj[6:9, 6:9] = R
+        
+        return adj
 
 def main():
     rclpy.init()
